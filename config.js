4ed8f075a40969a1444da1af1598da1b3ba44324
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-06-17 17:37:05 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,64 @@
+export const eventData = {
+    couple: {
+      names: "Genesis & Jorge",
+      date: "26 . 07 . 2025",
+      heroImage: "/images/2.png"
+    },
+    song: {
+      title: "Admv (cover) - Angie Camille",
+      file: "/Maluma - ADMV Cover Angie Camille.mp3"
+    },
+    ceremony: {
+      ceremonyImage:"/images/ceremon.png",
+      place: "Iglesia San Francisco",
+      address: "6-34, 13 Calle 1, Zona 1, Cdad. de Guatemala",
+      date: "26 de Julio",
+      time: "15:00 hrs",
+      mapLink: "https://maps.app.goo.gl/F3hRkoQM8zVMiW9u6"
+    },
+    reception: {
+      receptionImage:"/images/reception.png",
+      place: "Hotel Conquistador",
+      address: "Via 5 4-68, Zona 4, Cdad. de Guatemala",
+      date: "26 de Julio",
+      time: "18:00 hrs",
+      mapLink: "https://maps.app.goo.gl/qGEPHjSEeFPtQpVK7"
+    },
+
+    abroadGiftMessage: "Su presencia en nuestra boda es el mejor regalo que podríamos recibir. Si desean obsequiarnos algo más, una contribución económica será muy apreciada para ayudarnos a comenzar esta nueva etapa juntos.",
+    giftImage: "/images/gift.png",
+    
+    dresscode: {
+      description: "Traje formal, el blanco queda reservado exclusivamente para la novia.",
+      image: "/images/dresscode.png",
+      inspiration: {
+        women: "https://www.pinterest.com/search/pins/?q=Vestidos%20invitadas%20de%20boda%20de%20día",
+        men: "https://www.pinterest.com/search/pins/?q=Traje%20formal%20de%20hombres%20invitados%20a%20boda"
+      }
+    },  
+    gallery: [
+      "/images/V1.jpeg",
+      "/images/V2.jpeg",
+      "/images/V3.jpeg",
+      "/images/V4.jpeg",
+      "/images/H1.jpeg",
+    ],
+
+    noKidsPolicy: "Para disfrutar plenamente de esta celebración, nuestra boda será solo para adultos.",
+    finalPhoto: "/images/10.png",
+    finalMessage: "Porque el amor se celebra con quienes más queremos, esperamos contar con su presencia en este día inolvidable.",
+    rsvp: {
+      rsvpImage: "/images/rsvp.png",
+      whatsapp: "https://wa.me/50236011737?text=Confirmo%20mi%20asistencia%20a%20la%20boda",
+      email: "mailto:hello@thetwodesign.com"
+    },
+    footer: {
+      social: {
+        whatsapp: "https://wa.me/50236011737",
+        facebook: "https://www.facebook.com/thetwodesign",
+        instagram: "https://www.instagram.com/thetwodesign"
+      },
+      logo: "/images/logo.png",
+    }
+  };
+  
